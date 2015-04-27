86e5b5b590ed72983347bd013337b2a6b30f99d3
Daniel Lacis <danlacis@gmail.com>
2015-04-27 00:07:05 -0700

    authentication, composite view, serializejson

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,18 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.serializejson
+//= require moment
+//= require bootstrap-datetimepicker
+//= require underscore
+//= require backbone
+//= require composite_view
+//= require lila_fix_app
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree ./utils
 //= require_tree .
 //= require bootstrap
---
+++ app/assets/javascripts/lila_fix_app.js
@@ -0,0 +1,13 @@
+window.LilaFixApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  LilaFixApp.initialize();
+});
