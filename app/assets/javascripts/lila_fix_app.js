10931a52bcf0c6253bf650d46c999ea1fc1fa0ba
Daniel Lacis <danlacis@gmail.com>
2015-05-14 18:02:06 -0700

    finished changing namespace to LilaFixApp, ready to start backbone and rails content

--- app/assets/javascripts/lila_fix_app.js
+++ app/assets/javascripts/lila_fix_app.js
@@ -4,12 +4,8 @@ window.LilaFixApp = {
   Views: {},
   Routers: {},
   initialize: function() {
-    // var $rootEl = $('div#main');
-    // var $navEl = $('div#nav-el');
-    // var $modalEl = $('div#new-project-modal');
-    // var categories = new LaunchAssist.Collections.Categories();
-    // var currentUser = new LaunchAssist.Models.CurrentUser();
-    // var router = new LaunchAssist.Routers.Router({currentUser: currentUser, categories: categories, $rootEl: $rootEl, $navEl:  $navEl, $modalEl: $modalEl});
+    var $rootEl = $('div#main');
+    var router = new LilaFixApp.Routers.Router({$rootEl: $rootEl});
     Backbone.history.start();
   }
 };
